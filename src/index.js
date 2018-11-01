34e018af710cfdfc0dc9438bca928f37d11bf01d
william <grisa.william@gmail.com>
2018-11-01 14:09:05 -0300

    Refactor code using good practices, add prettier and test watch

--- src/index.js
+++ src/index.js
@@ -1,72 +1,81 @@
-import { createAction } from 'redux-actions';
+import { createAction } from 'redux-actions'
 
 /**
  * Creates an async action creator
  *
- * @param  {String} TYPE    the type of the action
- * @param  {Function} fn    the function to be called async
- * @return {Funtion}        the action creator
+ * @param {String} type the type of the action
+ * @param {Function} asyncFunction the function to be called async
+ * @return {Function} the action creator
  */
-export function createActionThunk (type, fn) {
-  const TYPE_START     = `${type}_STARTED`;
-  const TYPE_SUCCEEDED = `${type}_SUCCEEDED`;
-  const TYPE_FAILED    = `${type}_FAILED`;
-  const TYPE_ENDED     = `${type}_ENDED`;
+export function createActionThunk(type, asyncFunction) {
+  const TYPE_START = `${type}_STARTED`
+  const TYPE_SUCCEEDED = `${type}_SUCCEEDED`
+  const TYPE_FAILED = `${type}_FAILED`
+  const TYPE_ENDED = `${type}_ENDED`
+
   const actionCreators = {
-    [TYPE_START]     : createAction(TYPE_START),
-    [TYPE_SUCCEEDED] : createAction(TYPE_SUCCEEDED),
-    [TYPE_FAILED]    : createAction(TYPE_FAILED),
-    [TYPE_ENDED]     : createAction(TYPE_ENDED)
-  };
-  const successActionWithMeta =
-    createAction(TYPE_SUCCEEDED, ({payload}) => payload, ({meta}) => meta);
-
-  const factory = (...args) => (dispatch, getState, extra) => {
-    let result;
-    let startedAt = (new Date()).getTime();
-    dispatch(actionCreators[TYPE_START](args));
-    const succeeded = (data) => {
-      const action = data && data.payload
-        ? successActionWithMeta(data)
-        : actionCreators[TYPE_SUCCEEDED](data);
-
-      dispatch(action);
-      let endedAt = (new Date()).getTime();
-      dispatch(actionCreators[TYPE_ENDED]({
-        elapsed: endedAt - startedAt
-      }));
-      return data;
-    };
-    const failed = (err) => {
-      let endedAt = (new Date()).getTime();
-      dispatch(actionCreators[TYPE_FAILED](err));
-      dispatch(actionCreators[TYPE_ENDED]({
-        elapsed: endedAt - startedAt
-      }));
-      throw err;
-    }
-    try {
-      result = fn(...args, {getState, dispatch, extra});
-    } catch (error) {
-      failed(error);
-    }
-    // in case of async (promise), use success and fail callbacks.
-    if (isPromise(result)) {
-      return result.then(succeeded, failed);
-    }
-    return succeeded(result);
+    [TYPE_START]: createAction(TYPE_START),
+    [TYPE_SUCCEEDED]: createAction(TYPE_SUCCEEDED),
+    [TYPE_FAILED]: createAction(TYPE_FAILED),
+    [TYPE_ENDED]: createAction(TYPE_ENDED),
   }
 
-  factory.NAME = type;
-  factory.START = actionCreators[TYPE_START].toString();
-  factory.SUCCEEDED = actionCreators[TYPE_SUCCEEDED].toString();
-  factory.FAILED = actionCreators[TYPE_FAILED].toString();
-  factory.ENDED = actionCreators[TYPE_ENDED].toString();
+  const successActionWithMeta = createAction(TYPE_SUCCEEDED, ({ payload }) => payload, ({ meta }) => meta)
 
-  return factory;
+  const factory = (...args) => (dispatch, getState, extra) => {
+    let result
+    let startedAt = new Date().getTime()
+
+    dispatch(actionCreators[TYPE_START](args))
+
+    const succeeded = (data) => {
+      const successAction = data && data.payload ? successActionWithMeta(data) : actionCreators[TYPE_SUCCEEDED](data)
+
+      dispatch(successAction)
+
+      let endedAt = new Date().getTime()
+      dispatch(
+        actionCreators[TYPE_ENDED]({
+          elapsed: endedAt - startedAt,
+        }),
+      )
+      return data
+    }
+
+    const failed = (error) => {
+      let endedAt = new Date().getTime()
+
+      dispatch(actionCreators[TYPE_FAILED](error.response ? error.response.data : error))
+
+      dispatch(
+        actionCreators[TYPE_ENDED]({
+          elapsed: endedAt - startedAt,
+        }),
+      )
+    }
+
+    try {
+      result = asyncFunction(...args, { getState, dispatch, extra })
+    } catch (error) {
+      failed(error)
+    }
+
+    if (isPromise(result)) {
+      return result.then(succeeded, failed)
+    }
+
+    return succeeded(result)
+  }
+
+  factory.NAME = type
+  factory.START = actionCreators[TYPE_START].toString()
+  factory.SUCCEEDED = actionCreators[TYPE_SUCCEEDED].toString()
+  factory.FAILED = actionCreators[TYPE_FAILED].toString()
+  factory.ENDED = actionCreators[TYPE_ENDED].toString()
+
+  return factory
 }
 
-//helpers
-function isPromise(p) {
-  return p && p.then && p.catch;
+function isPromise(object) {
+  return object && object.then && object.catch
 }
--- test/index.specs.js
+++ test/index.specs.js
@@ -1,126 +1,120 @@
 /* eslint-env node, mocha */
 /* global Promise */
-import assert from 'assert';
-import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import configureMockStore from 'redux-mock-store';
-import { createActionThunk } from '../src';
+import assert from 'assert'
+import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import configureMockStore from 'redux-mock-store'
+
+import { createActionThunk } from '../src'
 
 const reducer = (state = { started: false, data: null, error: null }, action) => {
-  console.log(action.type, action.payload, action.error); //eslint-disable-line
-  switch(action.type) {
+  switch (action.type) {
     case 'FETCH_STARTED':
       return Object.assign({}, state, {
         started: true,
         error: null,
-        data: null
-      });
+        data: null,
+      })
     case 'FETCH_FAILED':
       return Object.assign({}, state, {
-        error: action.error
-      });
+        error: action.payload,
+      })
     case 'FETCH_SUCCEEDED':
       return Object.assign({}, state, {
-        data: action.payload
-      });
+        data: action.payload,
+      })
     case 'FETCH_ENDED':
       return Object.assign({}, state, {
-        started: false
-      });
+        started: false,
+      })
     default:
-      return state;
+      return state
   }
 }
 
 describe('createActionThunk', () => {
-  beforeEach(function () {
-    this.store = createStore(
-        reducer,
-        applyMiddleware(...[thunkMiddleware])
-    );
-  });
+  beforeEach(function() {
+    this.store = createStore(reducer, applyMiddleware(...[thunkMiddleware]))
+  })
 
-  it('should dispatch non async functions', function () {
-    let fetch = createActionThunk('FETCH', () => 3);
-    this.store.dispatch(fetch());
+  it('should dispatch non async functions', function() {
+    let fetch = createActionThunk('FETCH', () => 3)
+    this.store.dispatch(fetch())
     // assert.equal(this.store.getState().started, false);
-    assert.equal(this.store.getState().data, 3);
-  });
+    assert.equal(this.store.getState().data, 3)
+  })
 
-  it('should dispatch and return payload with non async functions', function () {
-    let fetch = createActionThunk('FETCH', () => 2);
-    let result = this.store.dispatch(fetch());
-    assert.equal(result, 2);
-  });
+  it('should dispatch and return payload with non async functions', function() {
+    let fetch = createActionThunk('FETCH', () => 2)
+    let result = this.store.dispatch(fetch())
+    assert.equal(result, 2)
+  })
 
-  it('should dispatch async function', function (done) {
-    let fetch = createActionThunk('FETCH', myAsyncFunc);
-    let promise = this.store.dispatch(fetch());
-    assert.equal(this.store.getState().started, true);
-    assert.equal(this.store.getState().data, null);
-    promise.then((data)=> {
-      assert.equal(this.store.getState().started, false);
-      assert.equal(this.store.getState().data, 10);
-      assert.equal(data, 10);
-      done();
-    }, done);
-  });
+  it('should dispatch async function', function(done) {
+    let fetch = createActionThunk('FETCH', myAsyncFunc)
+    let promise = this.store.dispatch(fetch())
+    assert.equal(this.store.getState().started, true)
+    assert.equal(this.store.getState().data, null)
+    promise.then((data) => {
+      assert.equal(this.store.getState().started, false)
+      assert.equal(this.store.getState().data, 10)
+      assert.equal(data, 10)
+      done()
+    }, done)
+  })
 
-  it('should dispatch FAILED, then ERROR and then throw on error', function () {
+  it('should dispatch FAILED and return the error', function() {
     let fetch = createActionThunk('FETCH', () => {
-      throw new Error('boom!');
-    });
-    try {
-      this.store.dispatch(fetch());
-    }
-    catch(e) {
-      assert.equal(e.message, 'boom!');
-      assert.equal(this.store.getState().started, false);
-      assert.equal(this.store.getState().error, true);
-    }
-  });
-  
-  it('should work with empty payload', function () {
+      throw new Error('boom!')
+    })
+
+    this.store.dispatch(fetch())
+
+    assert.equal(this.store.getState().started, false)
+    assert.equal(this.store.getState().error, 'Error: boom!')
+  })
+
+  it('should work with empty payload', function() {
     const middlewares = [thunkMiddleware] // add your middlewares like `redux-thunk`
-    const mockStore = configureMockStore(middlewares);
-    const store = mockStore({});
-    
-    let fetch = createActionThunk('FETCH', () => {});
-    store.dispatch(fetch());
+    const mockStore = configureMockStore(middlewares)
+    const store = mockStore({})
 
-    const actions = store.getActions();
-    assert.equal(actions.length, 3);
+    let fetch = createActionThunk('FETCH', () => {})
+    store.dispatch(fetch())
 
-    const [start, success, ended] = actions;
-    assert.deepEqual(start, {type: fetch.START, payload: []});
-    assert.deepEqual(success, {type: fetch.SUCCEEDED});
-    assert.equal(ended.type, fetch.ENDED);
-  });
+    const actions = store.getActions()
+    assert.equal(actions.length, 3)
 
-  describe('with meta', function () {
+    const [start, success, ended] = actions
+    assert.deepEqual(start, { type: fetch.START, payload: [] })
+    assert.deepEqual(success, { type: fetch.SUCCEEDED })
+    assert.equal(ended.type, fetch.ENDED)
+  })
+
+  describe('with meta', function() {
     const middlewares = [thunkMiddleware] // add your middlewares like `redux-thunk`
-    const mockStore = configureMockStore(middlewares);
+    const mockStore = configureMockStore(middlewares)
 
     beforeEach(() => {
-      this.store = mockStore({});
-    });
+      this.store = mockStore({})
+    })
 
     it('should dispatch action with meta', () => {
-      let fetch = createActionThunk('FETCH', () => ({payload: 2, meta: 3}));
-      this.store.dispatch(fetch(1));
+      let fetch = createActionThunk('FETCH', () => ({ payload: 2, meta: 3 }))
+      this.store.dispatch(fetch(1))
 
-      const actions = this.store.getActions();
-      assert.equal(actions.length, 3);
+      const actions = this.store.getActions()
+      assert.equal(actions.length, 3)
 
-      const [start, success, ended] = actions;
-      assert.deepEqual(start, {type: fetch.START, payload: [1]});
-      assert.deepEqual(success, {type: fetch.SUCCEEDED, payload: 2, meta: 3});
-      assert.equal(ended.type, fetch.ENDED);
-    });
-  });
-});
+      const [start, success, ended] = actions
+      assert.deepEqual(start, { type: fetch.START, payload: [1] })
+      assert.deepEqual(success, { type: fetch.SUCCEEDED, payload: 2, meta: 3 })
+      assert.equal(ended.type, fetch.ENDED)
+    })
+  })
+})
 
 // helpers
 function myAsyncFunc() {
-  return new Promise(resolve => setTimeout(()=>resolve(10), 50));
+  return new Promise((resolve) => setTimeout(() => resolve(10), 50))
 }
